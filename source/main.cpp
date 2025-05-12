8cf9079158918d76359bd5aa8f71f64477b65754
JiangKevin <JiangKevin@users.noreply.github.com>
2025-05-12 18:18:56 +0800

    备份一下，增加了融合算法的参数调整功能

--- source/main.cpp
+++ source/main.cpp
@@ -219,40 +219,40 @@ int main()
                 //
                 ahrs_calculation_.gyroscopeMisalignment = { std::stof( values[ 1 ] ), std::stof( values[ 2 ] ), std::stof( values[ 3 ] ), std::stof( values[ 4 ] ), std::stof( values[ 5 ] ), std::stof( values[ 6 ] ), std::stof( values[ 7 ] ), std::stof( values[ 8 ] ), std::stof( values[ 9 ] ) };
                 //
-                ahrs_calculation_.gyroscopeSensitivity = { std::stof( values[ 10 ] ), std::stof( values[ 11 ] ), std::stof( values[ 13 ] ) };
+                ahrs_calculation_.gyroscopeSensitivity = { std::stof( values[ 10 ] ), std::stof( values[ 11 ] ), std::stof( values[ 12 ] ) };
                 //
-                ahrs_calculation_.gyroscopeOffset = { std::stof( values[ 14 ] ), std::stof( values[ 15 ] ), std::stof( values[ 16 ] ) };
+                ahrs_calculation_.gyroscopeOffset = { std::stof( values[ 13 ] ), std::stof( values[ 14 ] ), std::stof( values[ 15 ] ) };
                 //
-                ahrs_calculation_.accelerometerMisalignment = { std::stof( values[ 17 ] ), std::stof( values[ 18 ] ), std::stof( values[ 19 ] ), std::stof( values[ 20 ] ), std::stof( values[ 21 ] ),
-                                                                std::stof( values[ 22 ] ), std::stof( values[ 23 ] ), std::stof( values[ 24 ] ), std::stof( values[ 25 ] ) };
+                ahrs_calculation_.accelerometerMisalignment = { std::stof( values[ 16 ] ), std::stof( values[ 17 ] ), std::stof( values[ 18 ] ), std::stof( values[ 19 ] ), std::stof( values[ 20 ] ),
+                                                                std::stof( values[ 21 ] ), std::stof( values[ 22 ] ), std::stof( values[ 23 ] ), std::stof( values[ 24 ] ) };
                 //
-                ahrs_calculation_.accelerometerSensitivity = { std::stof( values[ 26 ] ), std::stof( values[ 27 ] ), std::stof( values[ 28 ] ) };
-                ahrs_calculation_.accelerometerOffset      = { std::stof( values[ 29 ] ), std::stof( values[ 30 ] ), std::stof( values[ 31 ] ) };
+                ahrs_calculation_.accelerometerSensitivity = { std::stof( values[ 25 ] ), std::stof( values[ 26 ] ), std::stof( values[ 27 ] ) };
+                ahrs_calculation_.accelerometerOffset      = { std::stof( values[ 28 ] ), std::stof( values[ 29 ] ), std::stof( values[ 30 ] ) };
                 //
-                ahrs_calculation_.softIronMatrix = { std::stof( values[ 32 ] ), std::stof( values[ 33 ] ), std::stof( values[ 34 ] ), std::stof( values[ 35 ] ), std::stof( values[ 36 ] ), std::stof( values[ 37 ] ), std::stof( values[ 38 ] ), std::stof( values[ 39 ] ), std::stof( values[ 40 ] ) };
+                ahrs_calculation_.softIronMatrix = { std::stof( values[ 31 ] ), std::stof( values[ 32 ] ), std::stof( values[ 33 ] ), std::stof( values[ 34 ] ), std::stof( values[ 35 ] ), std::stof( values[ 36 ] ), std::stof( values[ 37 ] ), std::stof( values[ 38 ] ), std::stof( values[ 39 ] ) };
                 //
-                ahrs_calculation_.hardIronOffset = { std::stof( values[ 41 ] ), std::stof( values[ 42 ] ), std::stof( values[ 43 ] ) };
+                ahrs_calculation_.hardIronOffset = { std::stof( values[ 40 ] ), std::stof( values[ 41 ] ), std::stof( values[ 42 ] ) };
                 //
 
                 //
-                if ( std::stoi( values[ 44 ] ) == 0 )
+                if ( std::stoi( values[ 43 ] ) == 0 )
                 {
                     ahrs_calculation_.settings.convention = FusionConventionNwu;
                 }
-                else if ( std::stoi( values[ 44 ] ) == 1 )
+                else if ( std::stoi( values[ 43 ] ) == 1 )
                 {
                     ahrs_calculation_.settings.convention = FusionConventionEnu;
                 }
-                else if ( std::stoi( values[ 44 ] ) == 2 )
+                else if ( std::stoi( values[ 43 ] ) == 2 )
                 {
                     ahrs_calculation_.settings.convention = FusionConventionNed;
                 }
                 //
-                ahrs_calculation_.settings.gain                  = std::stof( values[ 45 ] );
-                ahrs_calculation_.settings.gyroscopeRange        = std::stof( values[ 46 ] );
-                ahrs_calculation_.settings.accelerationRejection = std::stof( values[ 47 ] );
-                ahrs_calculation_.settings.magneticRejection     = std::stof( values[ 48 ] );
-                ahrs_calculation_.settings.recoveryTriggerPeriod = std::stoi( values[ 49 ] ) * SAMPLE_RATE;
+                ahrs_calculation_.settings.gain                  = std::stof( values[ 44 ] );
+                ahrs_calculation_.settings.gyroscopeRange        = std::stof( values[ 45 ] );
+                ahrs_calculation_.settings.accelerationRejection = std::stof( values[ 46 ] );
+                ahrs_calculation_.settings.magneticRejection     = std::stof( values[ 47 ] );
+                ahrs_calculation_.settings.recoveryTriggerPeriod = std::stoul( values[ 48 ] ) * SAMPLE_RATE;
             }
             //
             // 清空数据：销毁并重新创建
